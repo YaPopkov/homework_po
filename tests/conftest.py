c8f5d36b47c659c55f9c49e6575122d1f5b572cd
YaPopkov <popkov.rik@yandex.ru>
2023-05-14 19:10:27 +0300

    Fix url

--- tests/conftest.py
+++ tests/conftest.py
@@ -42,7 +42,7 @@ def setup_browser(request):
     password = os.getenv('PASSWORD')
 
     driver = webdriver.Remote(
-        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/",
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options
     )
     browser.config.driver = driver
